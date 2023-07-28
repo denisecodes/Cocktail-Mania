7ac5d7ae05500968218518282eacfa554c598c53
Denise Chan <cykdenise@gmail.com>
2023-07-28 23:53:55 +0100

    practice tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '8888883'
+        }
+      },
+      fontFamily: {
+        heading: ['Pacifico'],
+        body: ['Montserrat']
+      }
+    },
+  },
+  plugins: [],
+}
+
